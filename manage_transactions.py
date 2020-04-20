722a5d872d6ff060b3be13d13361f5465dd826ff
joerg.kiesewetter <joerg.kiesewetter@gmail.com>
2020-04-20 19:05:53 +0200

    fixed some bugs; added support for more transaction types

--- manage_transactions.py
+++ manage_transactions.py
@@ -5,7 +5,7 @@ import config
 from provider.terra import Terra
 from util import logging
 
-BASE_DIRECTORY = '/terra/raw/transactions/'
+BASE_DIRECTORY = '/terra/raw/transactions'
 
 log = logging.get_custom_logger(__name__, config.LOG_LEVEL)
 
@@ -40,11 +40,14 @@ def update_token_transactions():
 
     transactions = Terra.get_transaction(last_block)
 
+    _clear_last_block(last_block)
+
     max_time_exceeded = False
 
     while not max_time_exceeded:
 
-        _clear_last_block(transactions)
+        log.debug('storing block ' + str(last_block))
+
 
         for transaction in transactions:
 
@@ -63,10 +66,12 @@ def update_token_transactions():
 
             if type not in token.keys():
                 token[type] = {
-                    'directory': BASE_DIRECTORY + type,
+                    'directory': os.path.join(BASE_DIRECTORY, type.replace('/', '_')),
                     'file': None,
                     'filename': None
                 }
+
+                os.makedirs(token[type]['directory'], exist_ok=True)
 
             act_filename = timestamp.strftime('%Y-%m-%d') + '.csv'
             if not token[type]['file'] or act_filename != token[type]['filename']:
@@ -77,31 +82,141 @@ def update_token_transactions():
 
                 token[type]['file'] = open(os.path.join(token[type]['directory'], token[type]['filename']), 'a')
 
-            if type == 'oracle/MsgExchangeRateVote':
-                new_line = '/'.join([transaction['block'],
-                                     transaction['timestamp'],
+            if type == 'distribution/MsgWithdrawDelegationReward':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
                                      transaction['txhash'],
-                                     transaction['exchange_rate'],
+                                     transaction['delegator'],
+                                     transaction['validator'],
+                                    ])
+
+            elif type == 'distribution/MsgWithdrawValidatorCommission':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['validator'],
+                                    ])
+
+            elif type == 'gov/MsgSubmitProposal':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['proposer'],
+                                     transaction['init_deposit_amount'],
+                                     transaction['init_deposit_currency'],
+                                     transaction['proposal_id'],
+                                     transaction['proposal_title'],
+                                     transaction['proposal_text'],
+                                    ])
+
+            elif type == 'gov/MsgDeposit':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['depositor'],
+                                     transaction['proposal_id'],
+                                     transaction['amount'],
+                                     transaction['currency'],
+                                    ])
+
+            elif type == 'staking/MsgDelegate':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['delegator'],
+                                     transaction['validator'],
+                                     transaction['amount'],
+                                     transaction['currency'],
+                                    ])
+
+            elif type == 'market/MsgSwap':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['ask_address'],
+                                     transaction['ask_amount'],
+                                     transaction['ask_currency'],
+                                     transaction['bid_address'],
+                                     transaction['bid_amount'],
+                                     transaction['bid_currency'],
+                                    ])
+
+            elif type == 'staking/MsgEditValidator':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['address'],
+                                     transaction['details'],
+                                     transaction['moniker'],
+                                     transaction['website'],
+                                     transaction['identity'],
+                                     transaction['commission_rate'],
+                                     transaction.get('min_self_delegation') or '',
+                                    ])
+
+            elif type == 'staking/MsgCreateValidator':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     transaction['pubkey'],
+                                     transaction['amount'],
+                                     transaction['currency'],
+                                     transaction['commission_rate'],
+                                     transaction['commission_max_rate'],
+                                     transaction['commission_max_change_rate'],
+                                     transaction['details'],
+                                     transaction['moniker'],
+                                     transaction['website'],
+                                     transaction['identity'],
+                                     transaction['min_self_delegation'],
+                                     transaction['delegator'],
+                                     transaction['validator'],
+                                    ])
+
+            elif type == 'oracle/MsgExchangeRateVote':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     str(transaction['exchange_rate']),
                                      transaction['currency'],
                                     ])
 
             elif type == 'oracle/MsgExchangeRatePrevote':
-                new_line = '/'.join([transaction['block'],
-                                     transaction['timestamp'],
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
                                      transaction['txhash'],
                                      transaction['currency'],
                                     ])
 
+            elif type == 'oracle/MsgDelegateFeedConsent':
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                    ])
+
             elif type == 'bank/MsgMultiSend':
 
-                new_line = '/'.join([transaction['block'],
-                                     transaction['timestamp'],
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
                                      transaction['txhash'],
-                                     transaction['amount'],
+                                     str(transaction['amount']),
                                      transaction['currency'],
                                      transaction['from_address'],
                                      transaction['to_address'],
-                                     transaction['tax_amount'],
+                                     str(transaction['tax_amount']),
+                                     transaction['tax_currency'],
+                                    ])
+
+            elif type == 'bank/MsgSend':
+
+                new_line = ','.join([str(transaction['block']),
+                                     str(transaction['timestamp']),
+                                     transaction['txhash'],
+                                     str(transaction['amount']),
+                                     transaction['currency'],
+                                     transaction['from_address'],
+                                     transaction['to_address'],
+                                     str(transaction['tax_amount']),
                                      transaction['tax_currency'],
                                     ])
             else:
@@ -111,12 +226,13 @@ def update_token_transactions():
             token[type]['file'].write(new_line + '\n')
 
             last_timestamp = timestamp
-            last_block = block_number
+            # last_block = block_number
             last_hash = hash
 
         # log.debug('last block: ' + str(last_batch_block))
         # log.debug('last timestamp: ' + str(last_batch_timestamp))
-        transactions = Terra.get_transaction(last_block + 1)
+        last_block += 1
+        transactions = Terra.get_transaction(last_block)
 
         # if last_timestamp == last_batch_timestamp and last_block == last_batch_block and last_hash == last_batch_hash:
         #     break
@@ -133,15 +249,17 @@ def update_token_transactions():
                 token[key]['file'] = None
 
 
-def _clear_last_block(transactions):
+def _clear_last_block(block_number):
 
     directories = [f for f in os.listdir(BASE_DIRECTORY) if os.path.isdir(os.path.join(BASE_DIRECTORY, f))]
 
     for directory in directories:
+
+        path = os.path.join(BASE_DIRECTORY, directory)
         last_file_timestamp = None
         last_file = None
 
-        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
         # get the file with the highest timestamp
         for file in files:
@@ -156,20 +274,18 @@ def _clear_last_block(transactions):
         if not last_file:
             return
 
-
-        first_transaction = transactions[0]
         log.debug('removing data from the last block')
-        log.debug('scanning for block number: ' + first_transaction['blockNumber'])
+        log.debug('scanning for block number: ' + str(block_number) + ' in directory \'' + directory + '\'')
         removed_lines = 0
 
         new_lines = []
-        with open(os.path.join(directory, last_file), 'rt') as file:
+        with open(os.path.join(path, last_file), 'rt') as file:
 
             for line in file:
 
                 line_split = line.split(',')
 
-                if str(line_split[0]) != str(first_transaction['blockNumber']):
+                if str(line_split[0]) != str(block_number):
                     new_lines.append(line)
                 else:
                     removed_lines += 1
@@ -179,7 +295,7 @@ def _clear_last_block(transactions):
 
         log.debug('removing number of lines: ' + str(removed_lines))
 
-        with open(os.path.join(directory, last_file), 'w') as file:
+        with open(os.path.join(path, last_file), 'w') as file:
             for line in new_lines:
                 file.write(line)
             file.flush()
@@ -190,17 +306,18 @@ def _get_last_transaction():
 
     last_timestamp = 0
     # TODO change back to 0
-    last_block = 1607221
+    last_block = 0
     last_hash = None
 
     directories = [f for f in os.listdir(BASE_DIRECTORY) if os.path.isdir(os.path.join(BASE_DIRECTORY, f))]
 
     for directory in directories:
 
+        path = os.path.join(BASE_DIRECTORY, directory)
         last_file_timestamp = None
         last_file = None
 
-        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
         # get the file with the highest timestamp
         for file in files:
@@ -216,14 +333,18 @@ def _get_last_transaction():
         if not last_file:
             return 0, 0, None
 
+        # if the file exists, but is empty
+        if os.stat(os.path.join(path, last_file)).st_size <= 0:
+            continue
+
         # getting the last line of the file an extract the timestamp
-        with open(os.path.join(directory, last_file), 'rt') as file:
+        with open(os.path.join(path, last_file), 'rt') as file:
 
             last_line = file.readlines()[-1]
 
             last_line = last_line.split(',')
 
-            if last_block is None or last_block > last_line[1]:
+            if last_block is None or last_timestamp < int(last_line[1]):
                 last_timestamp = int(last_line[1])
                 last_block = int(last_line[0])
                 last_hash = last_line[2]
@@ -236,7 +357,7 @@ def get_first_transaction_timestamp(symbol):
 
     last_file_timestamp = None
 
-    dir = BASE_DIRECTORY + symbol
+    dir = os.path.join(BASE_DIRECTORY, symbol)
 
     files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 
--- provider/terra.py
+++ provider/terra.py
@@ -18,6 +18,7 @@ class Terra:
     def get_transaction(block: int):
         url = TERRA_BASE_URL + '?' + \
               'chainId=columbus-3' + \
+              '&limit=100' + \
               '&block=' + str(block)
 
         # request the first page
@@ -40,10 +41,14 @@ class Terra:
 
         for t in raw_transactions:
 
+            # when the transaction has a code, there is an error. So, we just ignore the whole transaction
+            if t.get('code'):
+                continue
+
             #
             # add transaction for transaction fee
             #
-            # TODO find out how to handle transaction fee
+            # TODO find out how to handle transaction fee (gas)
             # for fee in t['tx']['value']['fee']['amount']:
             #     final_transactions.append({
             #         'block': int(t['height']),
@@ -58,7 +63,7 @@ class Terra:
             # get taxes
             #
             tax_amount = 0
-            tax_currency = None
+            tax_currency = ''
 
             for log_entry in t['logs']:
 
@@ -76,7 +81,140 @@ class Terra:
             #
             for m in t['tx']['value']['msg']:
 
-                if m['type'] == 'oracle/MsgExchangeRateVote':
+                if m['type'] == 'distribution/MsgWithdrawDelegationReward':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'delegator': m['value']['delegator_address'],
+                        'validator': m['value']['validator_address'],
+                    })
+
+                elif m['type'] == 'distribution/MsgWithdrawValidatorCommission':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'validator': m['value']['validator_address'],
+                    })
+
+                elif m['type'] == 'gov/MsgSubmitProposal':
+
+                    proposal_id = -1
+
+                    for event in t['events']:
+                        if event['type'] == 'submit_proposal':
+                            proposal_id = event['attributes'][0]['value']
+
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'proposer': m['value']['proposer'],
+                        'init_deposit_amount': m['value']['initial_deposit'][0]['amount'],
+                        'init_deposit_currency': m['value']['initial_deposit'][0]['denom'],
+                        'proposal_id': proposal_id,
+                        'proposal_title': m['value']['content']['value']['title'],
+                        'proposal_text': m['value']['content']['value']['description'],
+                    })
+
+                elif m['type'] == 'gov/MsgDeposit':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'depositor': m['value']['depositor'],
+                        'proposal_id': m['value']['proposal_id'],
+                        'amount': m['value']['amount'][0]['amount'],
+                        'currency': m['value']['amount'][0]['denom'],
+                    })
+
+                elif m['type'] == 'staking/MsgDelegate':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'delegator': m['value']['delegator_address'],
+                        'validator': m['value']['validator_address'],
+                        'amount': m['value']['amount']['amount'],
+                        'currency': m['value']['amount']['denom'],
+                    })
+
+                elif m['type'] == 'market/MsgSwap':
+
+                    ask_address = ''
+                    bid_address = ''
+                    ask_amount = -1
+                    ask_currency = ''
+                    bid_amount = -1
+                    bid_currency = ''
+
+                    for event in t['events']:
+                        if event['type'] == 'transfer':
+
+                            ask_address = event['attributes'][2]['value']
+                            ask_amount = m['value']['offer_coin']['amount']
+                            ask_currency = m['value']['offer_coin']['denom']
+
+                            bid_address = event['attributes'][0]['value']
+                            bid_amount = event['attributes'][0]['value'][:-4]
+                            bid_currency = event['attributes'][0]['value'][-4:]
+
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'ask_address': ask_address,
+                        'ask_amount': ask_amount,
+                        'ask_currency': ask_currency,
+                        'bid_address': bid_address,
+                        'bid_amount': bid_amount,
+                        'bid_currency': bid_currency,
+                    })
+
+                elif m['type'] == 'staking/MsgEditValidator':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'address': m['value']['address'],
+                        'details': m['value']['Description']['details'],
+                        'moniker': m['value']['Description']['moniker'],
+                        'website': m['value']['Description']['website'],
+                        'identity': m['value']['Description']['identity'],
+                        'commission_rate': m['value']['commission_rate'],
+                        'min_self_delegation': m['value']['min_self_delegation'],
+                    })
+
+                elif m['type'] == 'staking/MsgCreateValidator':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'pubkey': m['value']['pubkey'],
+                        'amount': m['value']['value']['amount'],
+                        'currency': m['value']['value']['currency'],
+                        'commission_rate': m['value']['commission']['rate'],
+                        'commission_max_rate': m['value']['commission']['max_rate'],
+                        'commission_max_change_rate': m['value']['commission']['max_change_rate'],
+                        'details': m['value']['description']['details'],
+                        'moniker': m['value']['description']['moniker'],
+                        'website': m['value']['description']['website'],
+                        'identity': m['value']['description']['identity'],
+                        'min_self_delegation': m['value']['min_self_delegation'],
+                        'delegator': m['value']['delegator_address'],
+                        'validator': m['value']['validator_address'],
+                    })
+
+                elif m['type'] == 'oracle/MsgExchangeRateVote':
                     final_transactions.append({
                         'block': int(t['height']),
                         'txhash': t['txhash'],
@@ -93,6 +231,14 @@ class Terra:
                         'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
                         'type': m['type'],
                         'currency': m['value']['denom'],
+                    })
+
+                elif m['type'] == 'oracle/MsgDelegateFeedConsent':
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
                     })
 
                 elif m['type'] == 'bank/MsgMultiSend':
@@ -118,6 +264,22 @@ class Terra:
                             'tax_amount': int(round(tax_amount *  (amount / total_amount))),
                             'tax_currency': tax_currency,
                         })
+
+                elif m['type'] == 'bank/MsgSend':
+
+
+                    final_transactions.append({
+                        'block': int(t['height']),
+                        'txhash': t['txhash'],
+                        'timestamp': int(datetime.strptime(t['timestamp'], TIMESTAMP_FORMAT).timestamp()),
+                        'type': m['type'],
+                        'amount': int(m['value']['amount'][0]['amount']),
+                        'currency': m['value']['amount'][0]['denom'],
+                        'from_address': m['value']['from_address'],
+                        'to_address': m['value']['to_address'],
+                        'tax_amount': int(tax_amount),
+                        'tax_currency': tax_currency,
+                    })
 
                 else:
                     log.warning('transaction type not known: ' + m['type'])
